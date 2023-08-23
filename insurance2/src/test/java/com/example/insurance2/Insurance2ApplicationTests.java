2c3fe52f70cd7dda40bfa7dab039a3a4bc2392ce
ferasalshmrani <ferasalshmrani1@gmail.com>
2023-08-23 13:07:25 +0300

    git ignore

---
+++ insurance2/src/test/java/com/example/insurance2/Insurance2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.insurance2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Insurance2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
